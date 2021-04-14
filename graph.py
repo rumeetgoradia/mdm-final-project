e25407215f2f9689a8df3872f571d55f0190b68b
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-04-14 19:45:54 -0400

    Add save functionality for graphs

--- graph.py
+++ graph.py
@@ -1,19 +1,25 @@
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+
+from constants import NUM_NODES
 
 
 def create_graph():
     citations_file = open("./data/citations.dat")
-    numNodes = len(citations_file.readlines())
-    A = np.zeros(shape=(numNodes, numNodes), dtype=int)
+    A = np.zeros(shape=(NUM_NODES, NUM_NODES), dtype=int)
     for i, line in enumerate(citations_file):
         split_line = line.split(" ")
         if len(split_line) > 1:
-            for j in range(1, int(split_line[0])):
-                A[i, j] = 1
-    G = nx.from_numpy_matrix(A)
-    return G
+            for j in range(1, int(split_line[0]) + 1):
+                A[i, int(split_line[j])] = 1
+    citations_file.close()
+    return nx.from_numpy_matrix(A)
 
 
-if __name__ == "__main__":
-    create_graph()
+def write_graph(G, filepath="./graph.txt"):
+    nx.write_adjlist(G, filepath)
+
+
+def read_graph(filepath="./graph.txt"):
+    return nx.read_adjlist(filepath)

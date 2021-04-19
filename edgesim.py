01ecc23c4b3698c75afb14c12db7dc8f8228077f
Rohan Shah <rohanshah01@gmail.com>
2021-04-19 17:23:25 -0400

    Create function call
    created function call, added save to text file option

--- edgesim.py
+++ edgesim.py
@@ -3,15 +3,24 @@ import numpy as np
 import networkx as nx
 from constants import NUM_NODES
 
-g = graph.create_graph() #could be done also from adj matrix
-
-js_edges_np_matrix = np.zeroes(NUM_NODES)
-l = range(0,NUM_NODES)
-for i in l:
-    ni = set(g[i])
-    for j in l:
-        if (i >= j):
-            continue
-        nj= set(g[j])
-        js_edges_np_matrix[i,j] = len(ni.intersection(nj))/len(ni.union(nj))
+def edgesimilarity(g):
+    js_edges_np_matrix = np.zeros((NUM_NODES,NUM_NODES))
+    l = range(0,NUM_NODES)
+    for i in l:
+        print(str(i) + " of " + str(NUM_NODES))
+        ni = set(g[i])
+        if(len(ni) != 0):
+            for j in l:
+                if (i >= j):
+                    continue
+                nj= set(g[j])
+                js_edges_np_matrix[i,j] = len(ni.intersection(nj))/len(ni.union(nj))
+    return js_edges_np_matrix
         
+
+g = graph.create_graph() #could be done also from adj matrix
+sol = edgesimilarity(g)
+
+np.savetxt('t.txt',sol,delimiter=',',fmt='%.2e')
+
+#might make sense to just change the weight of each edge to the jc. sim, then return the graph

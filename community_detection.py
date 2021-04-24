d148b7247908f2cc2891faf005c72c4dc5bfe31f
Rumeet Goradia <rumeet.goradia@gmail.com>
2021-04-24 17:26:16 -0400

    Try community detection

---
+++ community_detection.py
@@ -0,0 +1,66 @@
+import csv
+import os
+import sys
+from pathlib import Path
+
+# pip install python-modularity-maximization==0.0.1rc4
+from modularity_maximization import partition
+from modularity_maximization.utils import get_modularity
+from networkx.algorithms import community
+
+from common import check_input_valid
+from constants import DIRECTORY
+from graph import read_graph
+
+
+def get_communities(G):
+    # communities_generator = community.girvan_newman(G)
+    # communities = next(communities_generator)
+    # modularity = community.modularity(G, communities)
+
+    # while (modularity < 0.5):
+    #     print("MODULARITY:", str(modularity))
+    #     prev_modularity = modularity
+    #     prev_communities = communities
+    #     communities = next(communities_generator)
+    #     modularity = community.modularity(G, communities)
+    #     if (modularity < prev_modularity):
+    #         communities = prev_communities
+    #         break
+
+    # return np.array([list(c) for c in communities])
+    communities = partition(G)
+    for comm in set(communities.values()):
+        print("Community %d" % comm)
+        print(', '.join([node for node in communities if communities[node] == comm]))
+    print("Modularity: %.3f" % get_modularity(G, communities))
+    return communities
+
+
+def write_communities(dataset: str, communities: dict):
+    data_directory = os.path.join(DIRECTORY, "communities")
+    Path(data_directory).mkdir(parents=True, exist_ok=True)
+    # np.save(os.path.join(data_directory, dataset + "-communities.npy"), communities)
+    with open(os.path.join(data_directory, dataset + "-communities.npy"), "w") as communities_file:
+        writer = csv.writer(communities_file)
+        for node, comm in communities.items():
+            writer.writerow([node, comm])
+
+
+def read_communities(dataset: str):
+    communities = {}
+    data_directory = os.path.join(DIRECTORY, "communities")
+    with open(os.path.join(data_directory, dataset + "-communities.npy"), "r") as communities_file:
+        reader = csv.reader(communities_file)
+        for row in reader:
+            communities[row[0]] = row[1]
+
+    return communities
+
+
+if __name__ == "__main__":
+    dataset = check_input_valid(sys.argv)
+    G = read_graph(dataset)
+
+    communities = get_communities(G)
+    write_communities(dataset, communities)
